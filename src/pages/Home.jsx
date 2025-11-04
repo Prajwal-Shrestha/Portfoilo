5d99d39399be93e5c751ba9b30abc4dabb298007
Prajwal-Shrestha <prajwalshrestha2468@gmail.com>
2025-11-04 23:12:58 +0545

    Navbar Finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import { Navbar } from "../components/Navbar";
 import { StarBackground } from "../components/starbackground";
 
 export const Home = () => {
@@ -11,7 +12,7 @@ export const Home = () => {
         {/* Background Effects */}
             <StarBackground />
         {/* Navbar */}
-
+            <Navbar />
         {/* Main Content */}
 
         {/* Footer */}
