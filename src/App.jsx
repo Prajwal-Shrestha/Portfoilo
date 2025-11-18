87072c02239e654f964af000cb5d7c701dc35511
Prajwal-Shrestha <prajwalshrestha2468@gmail.com>
2025-11-18 15:32:59 +0545

    Fix filename casing: pages/Home.jsx -> pages/home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../src/pages/home.jsx";
+import Home from "./pages/Home.jsx";
 import { NotFound } from "./pages/NotFound"
 
 function App() {
---
+++ src/pages/home.jsx
@@ -0,0 +1,35 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { Navbar } from "../components/Navbar";
+import { StarBackground } from "../components/starbackground";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { Education } from "../components/Education";
+import { SkillsSection } from "../components/SkillsSection";
+import  Projects  from "../components/Projects";
+import { ContactSection } from "../components/ContactSection";
+
+export const Home = () => {
+    return(
+     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Theme Toggle */}
+            <ThemeToggle />
+
+
+        {/* Background Effects */}
+            <StarBackground />
+        {/* Navbar */}
+            <Navbar />
+        {/* Main Content */}
+            <main>
+                <HeroSection />
+                <AboutSection />
+                <Education />
+                <SkillsSection />
+                <Projects />
+                <ContactSection />
+            </main>
+        {/* Footer */}
+        <Footer />
+    </div>
+    );
+};
