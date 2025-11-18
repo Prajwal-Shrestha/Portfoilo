2580f1b34e7c4041a00c33514ee7f822ab1069a0
Prajwal-Shrestha <prajwalshrestha2468@gmail.com>
2025-11-19 00:15:13 +0545

    Fixing navbar

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ import { AnimatePresence } from "framer-motion";
 
 import Intro from "../components/Intro"; // adjust path if needed
 import { ThemeToggle } from "../components/ThemeToggle";
-import { Navbar } from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import StarBackground from "../components/StarBackground"; 
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
